f7c391a72ba306d63f67bc537b92d3a107448e48
KJ <19541224+reddpy@users.noreply.github.com>
2023-11-14 13:06:02 -0500

    alterations to the marketplace endpoint and added todos

--- app/server.ts
+++ app/server.ts
@@ -1,5 +1,5 @@
-import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import Fastify from "fastify";
 
 const prisma = new PrismaClient();
 
@@ -21,12 +21,41 @@ fastify.get("/marketplace", async function handler(request, reply) {
     },
   });
 
-  return { portfolios };
+  let USDollar = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  });
+
+  //TODO: get asking price by summing portfolio itemized assets
+  //TODO: get number of assets
+  //TODO: get list of types of assets
+  //TODO: get portfolio categories from  assets
+  //TODO: add sector to the organization
+  const constructed_payload = portfolios.map((portfolio_obj) => {
+    return {
+      id: portfolio_obj.id,
+      display_url: portfolio_obj.display_img_url
+        ? portfolio_obj.display_img_url
+        : "https://www.logo.wine/a/logo/WeWork/WeWork-Icon-Logo.wine.svg",
+      asking_price: USDollar.format(portfolio_obj.portfolio_price),
+      company_name: portfolio_obj.organization.name,
+      investors: portfolio_obj.organization.OrganizationInvestors.map(
+        (investor_obj) => {
+          return investor_obj.investor.name;
+        }
+      ),
+      description: portfolio_obj.description,
+      status: portfolio_obj.organization.organizationStatusId,
+    };
+  });
+
+  return { portfolios: constructed_payload };
 });
 
 async function startServer() {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 4000 });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
