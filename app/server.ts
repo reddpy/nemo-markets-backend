26cc4171fb296efc52cb86ee5db23b5f1cad21bf
KJ <19541224+reddpy@users.noreply.github.com>
2023-11-19 12:18:36 -0500

    created portfolio page endpoint

--- app/server.ts
+++ app/server.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import Fastify from "fastify";
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { USDollar } from "./utils/currency";
 
 const prisma = new PrismaClient();
@@ -7,6 +7,84 @@ const prisma = new PrismaClient();
 const fastify = Fastify({
   logger: true,
 });
+
+fastify.get(
+  "/portfolio/:portfolio_unique_id",
+  async function handler(
+    request: FastifyRequest<{ Params: { portfolio_unique_id: string } }>,
+    reply: FastifyReply
+  ) {
+    const portfolio_unique_id = request.params["portfolio_unique_id"];
+    console.log("portfolio Id", portfolio_unique_id);
+
+    const portfolio = await prisma.portfolio.findFirst({
+      where: { id: 1 }, //TODO: search by unique profile ID added later
+      include: {
+        organization: {
+          select: {
+            status: true,
+            sector: true,
+            name: true,
+            about: true,
+            stage: true,
+            headquarters: true,
+            year_founded: true,
+            OrganizationInvestors: {
+              select: {
+                investor: true,
+              },
+            },
+          },
+        },
+        assets: true,
+      },
+    });
+
+    const display_url = portfolio?.display_img_url
+      ? portfolio.display_img_url
+      : "https://www.logo.wine/a/logo/WeWork/WeWork-Logo.wine.svg";
+
+    const portfolio_pricing = portfolio!.assets.reduce(
+      (asking_price, currentValue) => asking_price + currentValue.asking_price,
+      0
+    );
+
+    const asset_types_list = [
+      ...new Set(
+        portfolio!.assets.map((asset) => {
+          return asset.category;
+        })
+      ),
+    ];
+
+    const investor_str_list = portfolio?.organization.OrganizationInvestors.map(
+      (investor_obj) => {
+        return investor_obj.investor.name;
+      }
+    );
+
+    const constructed_payload = await {
+      id: portfolio?.id,
+      display_url: display_url,
+      description: portfolio?.description,
+      company_about: portfolio?.organization.about,
+      portfolio_price: USDollar.format(portfolio_pricing),
+      portfolio_price_numeric: portfolio_pricing,
+      asset_count: portfolio!.assets.length,
+      asset_types: asset_types_list,
+      status: portfolio?.organization.status,
+      investors: investor_str_list,
+      sector: portfolio?.organization.sector,
+      company_name: portfolio?.organization.name,
+      assets: portfolio?.assets,
+      stage: portfolio?.organization.stage,
+      headquarters: portfolio?.organization.headquarters,
+      year_founded: portfolio?.organization.year_founded,
+    };
+
+    return constructed_payload;
+  }
+);
 
 fastify.get("/marketplace", async function handler(request, reply) {
   //default case
